e345d1624ec7f52759923b80fdc96c5dd1a3461c
DanielG2602 <daniel.comercial0981@.gmail.com>
2025-06-02 09:48:29 -0300

    startando

---
+++ src/main/java/com/AprendendoJava/Javinha/JavinhaApplication.java
@@ -0,0 +1,13 @@
+package com.AprendendoJava.Javinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AprendendoJava/Javinha/JavinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AprendendoJava.Javinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
